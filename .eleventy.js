f3d79fd7579148b23e2b96bdd31ed062f0f5ae7f
dhruvang <dhruvangg@gmail.com>
2023-12-13 18:31:41 +0530

    added sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
 const inspect = require("util").inspect;
+const format = require('date-fns/format')
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles/main.css");
   eleventyConfig.addPassthroughCopy("assets/img");
 
   eleventyConfig.addFilter("debug", (content) => `<pre>${inspect(content)}</pre>`);
+  eleventyConfig.addFilter('date', function (date, dateFormat) {
+    return format(date, dateFormat)
+  })
 
   // eleventyConfig.addFilter('topicFilter', function (collection, topic) {
   //   if (!topic) return collection;
