ef7490310fc5150c0ce8b77e09fff956bae9e3b1
dhruvang <dhruvangg@gmail.com>
2024-02-08 17:46:42 +0530

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,12 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ 
-  weight: ['300', '500', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap'
-});
+// const inter = Inter({ 
+//   weight: ['300', '500', '700'],
+//   style: ['normal'],
+//   subsets: ['latin'],
+//   display: 'swap'
+// });
 export const metadata = {
   title: "dhruvang.com",
   description: "Dhruvang Gajjar, a software engineer with 6+ YoE in web development industry. Product Management, Problem solving and requiement understanding expert.",
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} overflow-x-hidden scroll-smooth`} >{children}</body>
+      <body className={`overflow-x-hidden scroll-smooth`} >{children}</body>
     </html>
   );
 }
