9e81296b07846512acfb27ef95c07c15f696614b
dhruvang <dhruvangg@gmail.com>
2024-02-08 18:07:29 +0530

    moved rendering to client component

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,9 +1,6 @@
 import { getArticle, getAllArticles } from "@/lib/api";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { notFound } from "next/navigation";
-import { MARKS, BLOCKS, INLINES } from "@contentful/rich-text-types";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
 import { Footer, Header } from "@/components/common";
 import Banner from "@/components/blog/Banner";
 import Content from "@/components/blog/Content";
@@ -21,20 +18,6 @@ export default async function KnowledgeArticle({ params }) {
         notFound();
     }
 
-    const options = {
-        renderMark: {
-            [MARKS.CODE]: (text) => {
-                return (
-                    <SyntaxHighlighter
-                        language="javascript"
-                        style={a11yDark}
-                    >
-                        {text}
-                    </SyntaxHighlighter>
-                );
-            }
-        },
-    };
     return (
         <main>
             <Header />
@@ -44,9 +27,7 @@ export default async function KnowledgeArticle({ params }) {
                 category={article.contentfulMetadata.tags}
                 image={article.image}
             />
-            <Content>
-                {documentToReactComponents(article.body.json, options)}
-            </Content>
+            <Content content={article.body.json}/>
             <Footer />
         </main>
     );
