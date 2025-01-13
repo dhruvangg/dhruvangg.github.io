fc017a48195fd987bed5ccec8ac90cf280909a33
Dhruvang <dhruvangg@gmail.com>
2025-01-13 17:38:13 +0530

    update layouts and added sitemap.xml

--- src/_includes/layout.11ty.js
+++ src/_includes/layout.11ty.js
@@ -1,43 +1,10 @@
+import Meta from "./meta.11ty.js";
 const Layout = (data) => {
     return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="${data.meta.description}" />
-        <meta name="keywords" content="${data.meta.keywords}" />
-        <meta name="author" content="${data.meta.author}" />
-        <meta name="language" content="English" />
-        <meta name="copyright" content="${data.meta.author}" />
-        <meta name="publisher" content="${data.meta.author}" />
-
-        <meta name="robots" content="index, follow" />
-        <meta name="googlebot" content="index, follow" />
-
-        <meta property="og:title" content="${data.title || data.meta.title}" />
-        <meta property="og:description" content="${data.meta.description}" />
-        <meta property="og:url" content="${data.meta.url}" />
-        <meta property="og:image" content="${data.ogImage || data.meta.image}" />
-
-        <meta property="twitter:title" content="${data.title || data.meta.title}" />
-        <meta property="twitter:description" content="${data.meta.description}" />
-        <meta property="twitter:url" content="${data.metaurl}" />
-        <meta property="twitter:image" content="${data.twitterImage || data.meta.image}" />
-        <meta property="twitter:card" content="summary_large_image" />
-
-        <link rel="icon" type="image/x-icon" href="/favicon.ico">
-        
-        <title>${data.title || data.meta.title}</title>
-        <link rel="stylesheet" href="/assets/styles.css">
-
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WN9QWB8');</script>
-        <!-- End Google Tag Manager -->
+        ${Meta(data)}
     </head>
     <body>
         <!-- Google Tag Manager (noscript) -->
@@ -63,7 +30,7 @@ const Layout = (data) => {
                     </div>
                 </a>
                 <div class="flex items-center space-x-4 leading-5 sm:space-x-6">
-                    <div class="no-scrollbar hidden items-center space-x-4 sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
+                    <div class="no-scrollbar flex items-center space-x-4 sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
                         <a class="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400" href="/blog">Blog</a>
                         <a class="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400" href="/projects">Projects</a>
                         <a class="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400" href="/about">About</a>
