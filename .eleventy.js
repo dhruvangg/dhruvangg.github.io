217ac05fc294f423dc3395a0c9885695a6058cee
Dhruvang <dhruvangg@gmail.com>
2025-01-13 18:40:11 +0530

    added 404 and robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -3,20 +3,22 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
+    
     eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-		formats: ["avif", "webp", "jpeg"],
-		widths: ["auto"],
-		htmlOptions: {
-			imgAttributes: {
-				loading: "lazy",
-				decoding: "async",
-			},
-			pictureAttributes: {}
-		},
-	});
+        formats: ["avif", "webp", "jpeg"],
+        widths: ["auto"],
+        htmlOptions: {
+            imgAttributes: {
+                loading: "lazy",
+                decoding: "async",
+            },
+            pictureAttributes: {}
+        },
+    });
 
     eleventyConfig.addCollection("blogPosts", function (collectionApi) {
-        return collectionApi.getFilteredByGlob("./src/posts/*.md").reverse(); 
+        return collectionApi.getFilteredByGlob("./src/posts/*.md").reverse();
     });
     return {
         dir: {
