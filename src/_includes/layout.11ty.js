cc75df7e7462df48d8aa41443182e5c88dc740f1
Dhruvang <dhruvangg@gmail.com>
2025-01-13 14:45:30 +0530

    Added GTM

--- src/_includes/layout.11ty.js
+++ src/_includes/layout.11ty.js
@@ -30,8 +30,20 @@ const Layout = (data) => {
         
         <title>${data.title || data.meta.title}</title>
         <link rel="stylesheet" href="/assets/styles.css">
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WN9QWB8');</script>
+        <!-- End Google Tag Manager -->
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WN9QWB8"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
             <header class="flex items-center w-full bg-white dark:bg-transparent justify-between py-10">
                 <a class="break-words" aria-label="TailwindBlog" href="/">
